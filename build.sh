e60e9048607cdecefa90b708950ae696f5d4092e
Michael Aaron Murphy <mmstickman@gmail.com>
2018-10-30 09:50:24 -0600

    Add Pop PPA dependency

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ function update_submodules {
 }
 
 function merge_repo {
-	tail -n +6 $1/suites/bionic.toml >> build/suites/bionic.toml
-	tail -n +6 $1/suites/cosmic.toml >> build/suites/cosmic.toml
+	tail -n +6 $1/suites/bionic.toml | grep -v extra_repos >> build/suites/bionic.toml
+	tail -n +6 $1/suites/cosmic.toml | grep -v extra_repos >> build/suites/cosmic.toml
 	rsync -avz --exclude='LICENSE' \
 		--exclude='suites/' \
 		--exclude='README.md' \
