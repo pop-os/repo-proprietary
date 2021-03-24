b015494e1502fedfa711fcb1708e55a97ee5f244
Jeremy Soller <jeremy@system76.com>
2021-03-23 18:46:59 -0600

    Add hirsute to build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-SUITES=(bionic focal groovy)
+SUITES=(bionic focal groovy hirsute)
 
 function install_debrep {
 	LATEST="$(git ls-remote https://github.com/pop-os/debrepbuild | grep HEAD | cut -c-7)"
