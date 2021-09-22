2da38c2168042be99a6564f25fe35a762411b6ce
Jeremy Soller <jeremy@system76.com>
2021-09-22 10:26:54 -0600

    Remove groovy

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-SUITES=(bionic focal groovy hirsute impish)
+SUITES=(bionic focal hirsute impish)
 
 function install_debrep {
 	LATEST="$(git ls-remote https://github.com/pop-os/debrepbuild | grep HEAD | cut -c-7)"
