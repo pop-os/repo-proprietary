1fbeb7c034d9fd597ddf4f7d0c2f30130001a181
Jacob Kauffmann <jacob@system76.com>
2024-02-15 15:49:23 -0700

    Finish dropping Bionic (18.04)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-SUITES=(bionic focal impish jammy)
+SUITES=(focal impish jammy)
 
 function install_debrep {
 	LATEST="$(git ls-remote https://github.com/pop-os/debrepbuild | grep HEAD | cut -c-7)"
