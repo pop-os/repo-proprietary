c3b78377849dff80741e4bc5082c0fcb90e8d514
Jeremy Soller <jackpot51@gmail.com>
2025-10-21 18:05:15 -0600

    Add resolute

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-SUITES=(focal impish jammy noble)
+SUITES=(focal impish jammy noble resolute)
 
 function install_debrep {
 	LATEST="$(git ls-remote https://github.com/pop-os/debrepbuild | grep HEAD | cut -c-7)"
